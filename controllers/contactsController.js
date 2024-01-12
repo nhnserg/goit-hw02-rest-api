c93d2da78b2c4eb52ee263427e40f2ec02ae060e
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-01-12 22:11:32 +0100

    2 commit

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,7 +1,9 @@
+// controllers/contactsController.js
 const fs = require("fs").promises;
 const path = require("path");
+const Joi = require("joi");
 
-const contactsPath = path.join(__dirname, "..", "data", "contacts.json");
+const contactsPath = path.join(__dirname, "..", "contacts.json");
 
 const listContacts = async () => {
   try {
@@ -23,50 +25,67 @@ const getById = async (id) => {
   }
 };
 
-const removeContact = async (id) => {
-  try {
-    const contacts = await listContacts();
-    const updateContacts = contacts.filter(
-      (c) => c.id.toString() !== id.toString()
-    );
-    await fs.writeFile(contactsPath, JSON.stringify(updateContacts, null, 2));
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-};
-
 const addContact = async (newContact) => {
   try {
     const contacts = await listContacts();
-    const updatedContacts = [...contacts, newContact];
+    const updatedContacts = [
+      ...contacts,
+      { ...newContact, id: Date.now().toString() },
+    ];
     await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
-    return newContact;
+    return { ...newContact, id: updatedContacts.length };
   } catch (error) {
     console.log(`Error: ${error}`);
   }
 };
 
-const updateContact = async (contactId, updateContact) => {
+const removeContact = async (id) => {
+  try {
+    const contacts = await listContacts();
+    const updatedContacts = contacts.filter(
+      (c) => c.id.toString() !== id.toString()
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+    return { message: "contact deleted" };
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+const updateContact = async (contactId, updatedContact) => {
   try {
     const contacts = await listContacts();
     const index = contacts.findIndex(
       (c) => c.id.toString() === contactId.toString()
     );
+
     if (index !== -1) {
-      contacts[index] = { ...contacts[index], ...updateContact };
+      contacts[index] = { ...contacts[index], ...updatedContact };
       await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
       return contacts[index];
     }
-    return null; // як що нема
+
+    return null;
   } catch (error) {
     console.log(`Error: ${error}`);
   }
 };
 
+const validateContact = (contact) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+  });
+
+  return schema.validate(contact);
+};
+
 module.exports = {
   listContacts,
   getById,
-  removeContact,
   addContact,
+  removeContact,
   updateContact,
+  validateContact,
 };
---
+++ index.js
@@ -0,0 +1,29 @@
+// app.js
+
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const contactsRouter = require("./routes/api/contacts");
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/errorHandlers");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

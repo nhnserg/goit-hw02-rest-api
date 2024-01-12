073e682e5807ab3822c65fda8b207edaa71d39bd
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-01-12 22:11:32 +0100

    2 commit

---
+++ middlewares/errorHandlers.js
@@ -0,0 +1,15 @@
+// notFoundHandler
+const notFoundHandler = (req, res, next) => {
+  res.status(404).json({ message: "Not found" });
+};
+
+// errorHandler
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+};
+
+module.exports = {
+  notFoundHandler,
+  errorHandler,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,39 +1,83 @@
 const express = require("express");
 const router = express.Router();
-const {
-  listContacts,
-  getById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../../models/contacts");
+const contactsController = require("../../controllers/contactsController");
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Homework done!" });
+  try {
+    const contacts = await contactsController.listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const contact = await contactsController.getById(id);
+    if (contact) {
+      res.status(200).json(contact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/", validateMiddleware, (req, res) => {
+router.post("/", async (req, res, next) => {
   const newContact = req.body;
-  const result = addContact(newContact);
-  res.status(201).json(result);
-});
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+
+  const validationResult = contactsController.validateContact(newContact);
+  if (validationResult.error) {
+    return res.status(400).json({ message: "missing required name field" });
+  }
+
+  try {
+    const addedContact = await contactsController.addContact(newContact);
+
+    res.status(201).json(addedContact);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put("/:id", validateMiddleware, (req, res) => {
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const contact = await contactsController.getById(id);
+    if (contact) {
+      await contactsController.removeContact(id);
+      res.status(200).json({ message: "contact deleted" });
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
   const contactId = req.params.id;
-  const updatedContact = req.body;
-  const result = updateContact(contactId, updatedContact);
+  const updatedFields = req.body;
 
-  if (result) {
-    res.status(200).json(result);
-  } else {
-    res.status(404).json({ message: "Not found" });
+  if (!updatedFields) {
+    return res.status(400).json({ message: "missing fields" });
+  }
+
+  try {
+    const updatedContact = await contactsController.updateContact(
+      contactId,
+      updatedFields
+    );
+
+    if (updatedContact) {
+      res.status(200).json(updatedContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    next(error);
   }
 });
 

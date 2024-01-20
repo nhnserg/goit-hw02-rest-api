17303cc490be184c64774475c877631077d8dd7d
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-01-20 15:52:31 +0100

    Update contactsSchemas.js

--- services/contactsSchemas.js
+++ services/contactsSchemas.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const updateContactSchema = Joi.object({
-  name: Joi.string().min(1).max(12),
+  name: Joi.string().min(1).max(30),
   email: Joi.string().email().min(5).max(30),
   phone: Joi.string().min(5).max(20),
 })
